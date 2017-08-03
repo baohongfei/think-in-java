42076ea686c08afe75c235ff90c055751988c426
baohongfei <baohongfei@gmail.com>
2017-08-03 09:44:58 +0800

    fix

--- src/com/baohongfei/fun/MyTelNum.java
+++ src/com/baohongfei/fun/MyTelNum.java
@@ -5,12 +5,12 @@ package com.baohongfei.fun;
  */
 public class MyTelNum {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,4,0,3,6,1};
-        int[] index = new int[]{6,0,0,3,1,5,1,2,4,4,0};
+        int[] arr = new int[]{1, 2, 4, 5, 6, 8, 9};
+        int[] index = new int[]{0, 5, 4, 1, 4, 2, 2, 6, 5, 3, 5};
         String tel = "";
-        for (int i:index){
+        for (int i : index) {
             tel += arr[i];
         }
-        System.out.println("联系方式:"+tel);
+        System.out.println("联系方式:" + tel);
     }
 }
