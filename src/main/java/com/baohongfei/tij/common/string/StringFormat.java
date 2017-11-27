0040faa84a6d68e769bd84c7e93ecd05ce6e0852
baohongfei <baohongfei@gmail.com>
2017-11-27 21:44:41 +0800

    string formatted

---
+++ src/main/java/com/baohongfei/tij/common/string/StringFormat.java
@@ -0,0 +1,12 @@
+package com.baohongfei.tij.common.string;
+
+/**
+ * @author hofer.bhf
+ * created on 2017/11/22 6:39 PM
+ */
+public class StringFormat {
+    public static void main(String[] args) {
+        System.out.println(String.format("我的%sx%s想", 1L, null
+                , 3));
+    }
+}
