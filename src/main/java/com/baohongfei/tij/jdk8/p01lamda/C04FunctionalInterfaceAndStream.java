a96222e522acf84c8fcf8f7aaf4e3dc4c811b9cd
baohongfei <baohongfei@gmail.com>
2017-11-27 22:29:01 +0800

    alt commond L

--- src/main/java/com/baohongfei/tij/jdk8/p01lamda/C04FunctionalInterfaceAndStream.java
+++ src/main/java/com/baohongfei/tij/jdk8/p01lamda/C04FunctionalInterfaceAndStream.java
@@ -28,16 +28,16 @@ public class C04FunctionalInterfaceAndStream {
         };
         System.out.println(i2.getClass().getInterfaces()[0]);
 
-        new Thread(()-> System.out.println("hello thread")).start();
+        new Thread(() -> System.out.println("hello thread")).start();
 
-        List<String> list = Arrays.asList("hello","world","hello world");
+        List<String> list = Arrays.asList("hello", "world", "hello world");
 
-        list.forEach(item-> System.out.println(item.toUpperCase()));
+        list.forEach(item -> System.out.println(item.toUpperCase()));
 
         List<String> list2 = new ArrayList<>();
 
-        list.forEach(item->list2.add(item.toUpperCase()));
-        list2.forEach(item-> System.out.println(item));
+        list.forEach(item -> list2.add(item.toUpperCase()));
+        list2.forEach(item -> System.out.println(item));
 
         list.stream().map(item -> item.toUpperCase()).forEach(item -> System.out.println(item));
 
